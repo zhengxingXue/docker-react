9189bd6efbf016d7d5f4c9d3639c0e5f31256dec
zhengxingXue <zx252cornell@gmail.com>
2022-01-07 15:12:12 -0500

    add subtitle

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <h1>Hello World!</h1>
+        <h2>New Feature</h2>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
